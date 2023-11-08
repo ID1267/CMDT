f69c9de7281f46fd2bf820d396a493973804d758
ID1267 <150020885+ID1267@users.noreply.github.com>
2023-11-08 22:42:23 +0800

    Update __init__.py

--- simulation/train_code/architecture/__init__.py
+++ simulation/train_code/architecture/__init__.py
@@ -4,7 +4,7 @@ from .DAUHST import DAUHST
 def model_generator(method):
     if 'dstuf' in method:
         num_iter = int(method.split('_')[1][0])
-        model = DAUHST()
+        model = DAUHST(num_iterations=num_iter)
     else:
         print(f'Method {method} is not defined !')
     return model
